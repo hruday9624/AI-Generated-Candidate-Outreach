3eb5441fb8a70e1113683cfb3d43328cacc6e3df
Hruday Mouly <Hruday96@users.noreply.huggingface.co>
2024-12-02 07:13:25 +0000

    Update app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import google.generativeai as genai
+
+# App header
+st.header("Candidate Outreach Using Ai")
+
+# Retrieve the API key from Streamlit secrets
+GOOGLE_API_KEY = st.secrets["GEMINI_API_KEY"]
+
+# Configure the Google Generative AI API with your API key
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Input field for the medicine name
+st.subheader("Enter Candidate Details:")
+candidate_name = st.text_input('Candidate Name', '')
